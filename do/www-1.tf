21d488d92494e7a97647662e0235ab0e0848684c
Maxim Alferov <malferov44@gmail.com>
2017-12-21 17:28:08 +0100

    do provider setup cosmetic

--- do/www-1.tf
+++ do/www-1.tf
@@ -3,26 +3,27 @@ variable "pvt_key" {}
 variable "ssh_key_fp" {}
 
 resource "digitalocean_droplet" "www-1" {
-  image = "centos-7-x64"
-  name = "www-1"
-  region = "ams3"
-  size = "512mb"
+  image    = "centos-7-x64"
+  name     = "www-1"
+  region   = "ams3"
+  size     = "512mb"
   private_networking = true
   ssh_keys = [
     "${var.ssh_key_fp}"
   ]
 
   connection {
-    user = "root"
-    type = "ssh"
+    user        = "root"
+    type        = "ssh"
     private_key = "${file(var.pvt_key)}"
-    timeout = "1m"
+    timeout     = "1m"
   }
 
   provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
-      "sudo yum -y install nginx"
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx",
     ]
   }
 
