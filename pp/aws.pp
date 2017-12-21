293587da1df5a7119b4afe0d10b8ab9e60390219
Maxim Alferov <malferov44@gmail.com>
2017-12-21 18:58:06 +0100

    aws puppet module init

--- pp/aws.pp
+++ pp/aws.pp
@@ -1,17 +1,46 @@
 # export AWS_ACCESS_KEY_ID=your_access_key_id
 # export AWS_SECRET_ACCESS_KEY=your_secret_access_key
 # export AWS_REGION=eu-west-1
+# or ~/.aws/credentials
+#[default]
+#aws_access_key_id = id
+#aws_secret_access_key = key
 
-ec2_instance { 'name-of-instance':
-  ensure            => running,
-  region            => 'us-east-1',
-  availability_zone => 'us-east-1a',
-  image_id          => 'ami-123456', # you need to select your own AMI
-  instance_type     => 't2.micro',
-  key_name          => 'name-of-existing-key',
-  subnet            => 'name-of-subnet',
-  security_groups   => ['name-of-security-group'],
-  tags              => {
-    tag_name => 'value',
+ec2_securitygroup { 'pp-sg-www':
+  ensure      => present,
+#  ensure      => absent,
+  region      => 'us-west-2',
+#  vpc         => 'name-of-vpc',
+  description => 'a description of the group',
+  ingress     => [{
+    protocol  => 'tcp',
+    port      => 22,
+    cidr      => '0.0.0.0/0',
   },
+  {
+    protocol  => 'tcp',
+    port      => 80,
+    cidr      => '0.0.0.0/0',
+  }],
+  tags        => {
+    tag_name  => 'value',
+  },
+}
+
+ec2_instance { 'pp-instance':
+  ensure            => running,
+#  ensure            => absent,
+  region            => 'us-west-2',
+#  availability_zone => 'us-west-2a',
+  image_id          => 'ami-bf4193c7',
+  instance_type     => 't2.micro',
+#  key_name          => 'name-of-existing-key',
+  subnet            => 'subnet-us-west-2a',
+  security_groups   => ['pp-sg-www'],
+  tags              => {
+    tag_name => 'terraform',
+  },
+  require => [
+    Ec2_securitygroup['pp-sg-www'],
+  ]
 }
