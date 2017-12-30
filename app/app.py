b18e3549d4114516dd65adf1fd49dd6352a5dda2
Maxim Alferov <malferov44@gmail.com>
2017-12-30 02:12:23 +0100

    app service mode

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-#!/bin/env python
 from flask import Flask
 from flask_restful import Api, Resource
 from requests import put, get
 
 app = Flask(__name__)
 api = Api(app)
+
+class Root(Resource):
+    def get(self):
+        return {'data': 'welcome'}
 
 class Health(Resource):
     def get(self):
@@ -13,7 +16,8 @@ class Health(Resource):
 def query(route):
     return get('http://localhost:9200/{}'.format(route), auth=('elastic', 'changeme')).json()
 
+api.add_resource(Root, '/')
 api.add_resource(Health, '/health')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
