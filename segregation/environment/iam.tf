c6f9d8c43e4d63552b701cb05bf2fc1fdb8ddb60
Maxim Alferov <malferov44@gmail.com>
2018-02-06 19:09:07 +0100

    iam move role to environment module

---
+++ segregation/environment/iam.tf
@@ -0,0 +1,41 @@
+
+resource "aws_iam_policy" "env_policy" {
+  name   = "environment_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "env_role" {
+  name = "environment_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "AWS": "arn:aws:iam::488700729350:root"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "env_attach" {
+  role       = "${aws_iam_role.env_role.name}"
+  policy_arn = "${aws_iam_policy.env_policy.arn}"
+}
